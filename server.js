e16a989d7981d21f978b6a4ed7f53472af3f31e2
James Fisher <jfisher396@hotmail.com>
2020-07-19 14:28:07 -0700

    initial setup of server complete

--- server.js
+++ server.js
@@ -1,1 +1,23 @@
+//Required dependencies
 const express = require('express');
+const exphbs = require('express-handlebars');
+//PORT setup
+const PORT = process.env.PORT || 8080;
+
+const app = express();
+// Serves static content for the app from the "public" directory in the application directory.
+app.use(express.static('public'));
+// Parses application body as JSON
+app.use(express.urlencoded({ extended: true }));
+app.use(express.json());
+//Sets handlebars as the HTML engine
+app.engine("handlebars", exphbs({ defaultLayout : "main "}))
+app.set('view endine', 'handlebars');
+
+// const routes = require()
+
+// app.use(routes);
+
+app.listen(PORT, function() {
+    console.log("Now listening at localhost:" + PORT);
+})
