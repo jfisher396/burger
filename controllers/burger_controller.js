c7babd34a6eb3a3d6e15834f1fd33491221386a9
James Fisher <jfisher396@hotmail.com>
2020-07-20 18:45:25 -0700

    POST request issue resolved and devour button placed correctly

--- controllers/burger_controller.js
+++ controllers/burger_controller.js
@@ -14,6 +14,7 @@ router.get("/", function (req, res) {
 });
 
 router.post("/api/burgers", function (req, res) {
+    console.log(req.body);
     burger.insertOne(["burger_name"], [req.body.name], function (result) {
          
         // Send back the ID of the new quote
